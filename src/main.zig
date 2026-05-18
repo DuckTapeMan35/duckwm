aaacca7f02d2cb94cb8a2aa4017c63e5dc06881a
DuckTapeMan35 <luis.tomas.nogueira@gmail.com>
2026-05-18 18:13:18 +0100

    cleaner exit

--- src/main.zig
+++ src/main.zig
@@ -3,21 +3,40 @@ const duckwm = @import("duckwm");
 const WM = duckwm.WM;
 const Config = duckwm.Config;
 
-pub fn main() void {
-    mainImpl() catch |err| {
-        std.debug.print("Fatal error: {}\n", .{err});
-        std.process.exit(1);
-    };
+var global_display_ptr: usize = 0; // store as usize to avoid X11 type dependency
+
+fn signal_handler(sig: i32) callconv(.c) void {
+    _ = sig;
+    std.process.exit(1);
 }
 
 fn mainImpl() !void {
     var da = std.heap.DebugAllocator(.{}){};
     defer _ = da.deinit();
-    
+
     var wm = try WM.create(da.allocator());
     wm.current_graph = &wm.graph;
     defer wm.deinit();
-    
+
+    const handler = std.os.linux.Sigaction{
+        .handler = .{ .handler = @ptrCast(&signal_handler) },
+        .mask = std.mem.zeroes(std.os.linux.sigset_t),
+        .flags = 0,
+    };
+
+    _ = std.os.linux.sigaction(std.os.linux.SIG.SEGV, &handler, null);
+    _ = std.os.linux.sigaction(std.os.linux.SIG.ABRT, &handler, null);
+    _ = std.os.linux.sigaction(std.os.linux.SIG.FPE,  &handler, null);
+    _ = std.os.linux.sigaction(std.os.linux.SIG.ILL,  &handler, null);
+    _ = std.os.linux.sigaction(std.os.linux.SIG.BUS,  &handler, null);
+
     try Config.load(&wm);
     try wm.run();
+}
+
+pub fn main() void {
+    mainImpl() catch |err| {
+        std.debug.print("Fatal error: {}\n", .{err});
+        std.process.exit(1);
+    };
 }
